a72501abeb8a0f4b4e4544d2a67c73e3dae7d8cd
Charlie <charlielu05@gmail.com>
2021-07-07 20:13:15 +1000

    adding s3 public block resource

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,3 +1,10 @@
+resource "aws_s3_account_public_access_block" "wintermute_s3_block" {
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket" "wintermute" {
   bucket = "wintermute-84"
   acl    = "private"
