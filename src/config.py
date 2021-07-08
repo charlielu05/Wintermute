b6d5cc9895bded18a4b1ffb04d571dd88a0198dc
Charlie <charlielu05@gmail.com>
2021-07-08 13:34:23 +1000

    eda step working with s3

---
+++ src/config.py
@@ -0,0 +1,8 @@
+
+class Config():
+    S3_BUCKET = 'wintermute-84'
+    OBJECT_NAME = 'wintermute_data'
+    PROCESSED_FILENAME = (f"processed/{OBJECT_NAME}.csv")
+    LOCAL_INPUT_FILEPATH = (f'./data/{OBJECT_NAME}')
+    LOCAL_OUTPUT_FILEPATH = (f'./data/{PROCESSED_FILENAME}')
+    
